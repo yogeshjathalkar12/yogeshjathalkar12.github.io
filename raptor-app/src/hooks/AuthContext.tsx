d1db84006b1c86f9089aa8a02af77a0d4a06853c
YOGESH-LEGEND <yogeshjathalkar@gmail.com>
2026-08-18 12:31:49 +0530

    Fixing false  signout

--- raptor-app/src/hooks/AuthContext.tsx
+++ raptor-app/src/hooks/AuthContext.tsx
@@ -32,6 +32,7 @@ export function AuthProvider({ children }: { children: ReactNode }) {
 
   const signOut = async () => {
     await supabase.auth.signOut();
+    window.location.href = 'https://shoonyaorigins.com/ventures/raptor/';
   };
 
   return (
